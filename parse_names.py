0779ee5881a38bb4408c9e3a45eec0718e7dd0a5
ieatmotherboards <hendres7373@gmail.com>
2026-04-20 12:32:17 -0400

    refactor parsing logic and write to output file

--- parse_names.py
+++ parse_names.py
@@ -1,14 +1,18 @@
 import re
 
-names = set()
-
-with open("unparsed_names.txt") as file:
-    file = file.readlines()
-    for line in file:
-        name = re.search(r'(?<=<strong>).*(?=</strong>)', line)
-        if name:
-            names.add(str(name.group()))
 
 
 if __name__ == "__main__":
-    print(names)
+    names = set()
+
+    with open("unparsed_names.txt") as file:
+        file = file.readlines()
+        for line in file:
+            name = re.search(r'(?<=<strong>).*(?=</strong>)', line)
+            if name:
+                names.add(str(name.group()))
+
+    with open('parsed_names.txt', 'w') as f:
+        # f.write(name for name in names) # list comprehension didn't work here :(
+        for name in names:
+            f.write(name + '\n')
