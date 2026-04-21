4f83dec020be533110462f3c89a66e4db8e878a4
ieatmotherboards <hendres7373@gmail.com>
2026-04-21 13:04:17 -0400

    complete sentence building algo

--- generate_phrases.py
+++ generate_phrases.py
@@ -3,7 +3,8 @@ import random
 def generate_phrases(nouns: set, proper_nouns: set, verbs: set, determiners: set):
     output = []
     # generate 200 paraphrases
-    for _ in range(3):
+    for _ in range(200):
+        
         out = ""
 
         verb = random.choice(list(verbs))
@@ -48,22 +49,72 @@ def generate_phrases(nouns: set, proper_nouns: set, verbs: set, determiners: set
         # else:
         #     proper_nouns.remove(n2)
 
+        det_n1 = random.choice(list(determiners)).capitalize()
+        det_n2 = random.choice(list(determiners)).capitalize()
+
         out += "1, "
-        out += random.choice(list(determiners)).capitalize() + " " + n1 if noun_or_pn_1 == 0 else n1 
+        out += det_n1 + " " + n1 if noun_or_pn_1 == 0 else n1 
         out +=  " " + verb + " "
-        out += random.choice(list(determiners)) + " " + n2 if noun_or_pn_2 == 0 else n2
+        out += det_n2 + " " + n2 if noun_or_pn_2 == 0 else n2
         out += ", "
 
-        out += random.choice(list(determiners)).capitalize() + " " + n2 if noun_or_pn_2 == 0 else n2
+        out += det_n2 + " " + n2 if noun_or_pn_2 == 0 else n2
         out +=  " was " + verb + " by "
-        out += random.choice(list(determiners)) + " " + n1 if noun_or_pn_1 == 0 else n1
+        out += det_n1 + " " + n1 if noun_or_pn_1 == 0 else n1
 
 
         output.append(out)
 
     # generate 200 NOT paraphrases
-    for _ in range(200):
+    for _ in range(2):
         out = ""
+
+        verb = random.choice(list(verbs))
+        verbs.remove(verb)
+
+        n1 = None
+        n2 = None
+
+        # flip coin to determine if the first noun is regular or PN
+        noun_or_pn_1 = random.randint(0,1)
+        if noun_or_pn_1 == 0: # go with noun
+            n1 = random.choice(list(nouns))
+            nouns.remove(n1)
+            # n1 = random.choice(list(determiners)).capitalize() + " " + n1
+        
+        else: # proper noun
+            
+            n1 = random.choice(list(proper_nouns))
+            proper_nouns.remove(n1)
+
+            
+
+        # flip coin to determine if the second noun is regular or PN
+        noun_or_pn_2 = random.randint(0,1)
+
+        if noun_or_pn_2 == 0: # go with noun
+            n2 = random.choice(list(nouns))
+            nouns.remove(n2)
+            # n2 = random.choice(list(determiners)).capitalize() + " " + n2
+
+        
+        else: # proper noun
+            n2 = random.choice(list(proper_nouns))
+            proper_nouns.remove(n2)
+        
+        det_n1 = random.choice(list(determiners))
+        det_n2 = random.choice(list(determiners))
+
+        out += "0, "
+        out += det_n1.capitalize() + " " + n1 if noun_or_pn_1 == 0 else n1 
+        out +=  " " + verb + " "
+        out += det_n2 + " " + n2 if noun_or_pn_2 == 0 else n2
+        out += ", "
+
+        out += det_n2.capitalize() + " " + n2 if noun_or_pn_2 == 0 else n2 
+        out += " was who "
+        out +=  det_n1 + " " + n1 if noun_or_pn_1 == 0 else n1
+        out += " was " + verb + " by"
 
 
         output.append(out)
